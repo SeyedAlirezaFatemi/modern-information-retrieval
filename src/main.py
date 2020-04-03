ba13dd762208ab8479475e5a3b1aa791d0e65f1d
Alireza Fatemi <alirezafa@rocketmail.com>
2020-04-03 11:35:35 +0430

    Added FIELDS and construct_bigram_index.

--- src/main.py
+++ src/main.py
@@ -12,13 +12,14 @@ from src.utils import next_greater, binary_search
 
 sys.setrecursionlimit(10 ** 6)
 DocID = int
-
+Token = str
 text_preparer = TextPreparer()
+FIELDS = ["title", "text"]
 
 
 class Document:
-    title_tokens: List[str]
-    text_tokens: List[str]
+    title_tokens: List[Token]
+    text_tokens: List[Token]
 
     def __init__(self, doc_id: DocID, title: str, text: str):
         self.doc_id = doc_id
@@ -81,8 +82,8 @@ def create_documents(
 @dataclass
 class TokenIndexItem:
     posting_list: List[PostingListItem]
-    term_frequency: int
-    doc_frequency: int
+    term_frequency: Dict[str, int]
+    doc_frequency: Dict[str, int]
 
 
 def read_document(docs_path: str, doc_id: DocID) -> Optional[Document]:
@@ -90,46 +91,80 @@ def read_document(docs_path: str, doc_id: DocID) -> Optional[Document]:
     return document
 
 
+def create_new_token_index_item():
+    term_frequency = dict()
+    doc_frequency = dict()
+    for field in FIELDS:
+        term_frequency[field] = 0
+        doc_frequency[field] = 0
+    return TokenIndexItem([], term_frequency, doc_frequency)
+
+
 class CorpusIndex:
     def __init__(self, documents: List[Document]):
         self.documents = documents
         self.corpus_index = self.construct_index(documents)
+        self.bigram_index = self.construct_bigram_index()
 
     def construct_index(self, documents: List[Document]) -> Dict[str, TokenIndexItem]:
         corpus_index = dict()
         for document in tqdm(documents):
-            token_positional_list_item_dict, token_frequency_dict = self.create_token_positional_list_item_dict(
+            token_positional_list_item_dict, token_frequency_dict = self.analyse_document(
                 document
             )
             for token in token_positional_list_item_dict:
                 if token not in corpus_index:
-                    corpus_index[token] = TokenIndexItem([], 0, 0)
+                    corpus_index[token] = create_new_token_index_item()
                 corpus_index[token].posting_list.append(
                     token_positional_list_item_dict[token]
                 )
-                corpus_index[token].term_frequency += token_frequency_dict[token]
-                corpus_index[token].doc_frequency += 1
+            for field in FIELDS:
+                for token in token_frequency_dict[field]:
+                    corpus_index[token].term_frequency[field] += token_frequency_dict[
+                        field
+                    ][token]
+                    corpus_index[token].doc_frequency[field] += 1
 
         return corpus_index
 
-    def create_token_positional_list_item_dict(
+    def construct_bigram_index(self) -> Dict[str, List[str]]:
+        bigram_index = dict()
+        for token in self.corpus_index:
+            modified_token = f"${token}$"
+            for idx in range(len(modified_token) - 1):
+                bigram = modified_token[idx : idx + 2]
+                if bigram not in bigram_index:
+                    bigram_index[bigram] = []
+                bigram_index[bigram].append(token)
+        return bigram_index
+
+    def analyse_document(
         self, document: Document
-    ) -> Tuple[Dict[str, PostingListItem], Dict[str, int]]:
+    ) -> Tuple[Dict[str, PostingListItem], Dict[str, Dict[str, int]]]:
         token_positional_list_item_dict = dict()
         token_frequency_dict = dict()
-        for field in ["title", "text"]:
+        for field in FIELDS:
+            token_frequency_dict[field] = dict()
             for idx, token in enumerate(document.__getattribute__(f"{field}_tokens")):
                 if token not in token_positional_list_item_dict:
                     token_positional_list_item_dict[token] = PostingListItem(
                         document.doc_id
                     )
-                    token_frequency_dict[token] = 0
+                if token not in token_frequency_dict[field]:
+                    token_frequency_dict[field][token] = 0
                 token_positional_list_item_dict[token].add_to_positions(field, idx)
-                token_frequency_dict[token] += 1
+                token_frequency_dict[field][token] += 1
         return token_positional_list_item_dict, token_frequency_dict
 
     def get_posting_list(self, word: str) -> List[PostingListItem]:
         return self.corpus_index[word].posting_list
+
+    def get_words_with_bigram(self, bigram: str) -> List[Token]:
+        try:
+            tokens = self.bigram_index[bigram]
+        except KeyError:
+            return []
+        return tokens
 
     def add_document_to_indexes(self, docs_path: str, doc_id: DocID) -> None:
         # Check if document already exists
@@ -147,9 +182,7 @@ class CorpusIndex:
             print("Document not found!")
             return
         document_insertion_idx = next_greater(
-            self.documents,
-            document,
-            key=lambda x: x.doc_id,
+            self.documents, document, key=lambda x: x.doc_id
         )
         document_insertion_idx = (
             document_insertion_idx
@@ -157,7 +190,7 @@ class CorpusIndex:
             else len(self.documents)
         )
         self.documents.insert(document_insertion_idx, document)
-        token_positional_list_item_dict, token_frequency_dict = self.create_token_positional_list_item_dict(
+        token_positional_list_item_dict, token_frequency_dict = self.analyse_document(
             document
         )
         for token in token_positional_list_item_dict:
@@ -180,12 +213,17 @@ class CorpusIndex:
                 self.corpus_index[token].posting_list.insert(
                     insertion_idx, token_positional_list_item_dict[token]
                 )
-            self.corpus_index[token].term_frequency += token_frequency_dict[token]
-            self.corpus_index[token].doc_frequency += 1
+        # Update doc and term frequencies
+        for field in FIELDS:
+            for token in token_frequency_dict[field]:
+                self.corpus_index[token].term_frequency[field] += token_frequency_dict[
+                    field
+                ][token]
+                self.corpus_index[token].doc_frequency[field] += 1
         return
 
     def add_token_to_index(self, token: str) -> None:
-        self.corpus_index[token] = TokenIndexItem([], 0, 0)
+        self.corpus_index[token] = create_new_token_index_item()
 
     def delete_document_from_indexes(self, docs_path: str, doc_id: DocID) -> None:
         # Check if document exists
@@ -200,7 +238,7 @@ class CorpusIndex:
             print("Document not found!")
             return
         del self.documents[idx]
-        token_positional_list_item_dict, token_frequency_dict = self.create_token_positional_list_item_dict(
+        token_positional_list_item_dict, token_frequency_dict = self.analyse_document(
             document
         )
         for token in token_positional_list_item_dict:
@@ -215,10 +253,17 @@ class CorpusIndex:
                 if deletion_idx == -1:
                     continue
                 del self.corpus_index[token].posting_list[deletion_idx]
-                self.corpus_index[token].doc_frequency -= 1
-                self.corpus_index[token].term_frequency -= token_frequency_dict[token]
-                if len(self.corpus_index[token].posting_list) == 0:
-                    del self.corpus_index[token]
+        # Update doc and term frequencies
+        for field in FIELDS:
+            for token in token_frequency_dict[field]:
+                self.corpus_index[token].doc_frequency[field] -= 1
+                self.corpus_index[token].term_frequency[field] -= token_frequency_dict[
+                    field
+                ][token]
+        # Delete tokens with empty posting list
+        for token in token_positional_list_item_dict:
+            if len(self.corpus_index[token].posting_list) == 0:
+                del self.corpus_index[token]
 
     def save_index(self, destination: str) -> None:
         with open(destination, "wb") as f:
