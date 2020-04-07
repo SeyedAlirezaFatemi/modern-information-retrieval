95b31a25751bfeb9fd0ecbc4a4d16736e5d461f4
Alireza Fatemi <alirezafa@rocketmail.com>
2020-04-07 12:37:22 +0430

    Adding metrics.

---
+++ src/metrics/__init__.py
@@ -0,0 +1,23 @@
+from typing import List
+
+import numpy as np
+
+MAX_RETRIEVED = 15
+
+
+def r_precision(relevant: List[int], num_relevant_docs: int) -> float:
+    return np.sum(relevant[:num_relevant_docs]) / num_relevant_docs
+
+
+def precision(relevant: List[int]) -> float:
+    return np.sum(relevant) / len(relevant)
+
+
+def recall(relevant: List[int], num_relevant_docs: int) -> float:
+    return np.sum(relevant) / num_relevant_docs
+
+
+def f_measure(relevant: List[int], num_relevant_docs: int) -> float:
+    precision_val = precision(relevant)
+    recall_val = recall(relevant, num_relevant_docs)
+    return 2 * precision_val * recall_val / (precision_val + recall_val)
