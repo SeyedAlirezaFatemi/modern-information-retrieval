5d13bd4d25fc98139d7dbd0d48672acdff17f253
Alireza Fatemi <alirezafa@rocketmail.com>
2020-05-07 14:57:30 +0430

    :sparkles: EnglishTextPreprocessor Added.

--- src/models/TextPreprocessor.py
+++ src/models/TextPreprocessor.py
@@ -7,28 +7,99 @@ from typing import List
 
 class TextPreprocessor(ABC):
     def __init__(
-        self, stem: bool = True, del_punctuation: bool = True, lemmatize: bool = False
+        self,
+        stem: bool = True,
+        lemmatize: bool = False,
+        del_punctuation: bool = True,
+        del_stop_words: bool = True,
     ):
         self.stem = stem
         self.del_punctuation = del_punctuation
+        self.del_stop_words = del_stop_words
         self.lemmatize = lemmatize
+        self.punctuation_pattern = re.compile(
+            r'([؟!\?]+|\d[\d\.:\/\\]+\d|[:\.=,//،|}{؛»\]\)\}"«\[\(\{])'
+        )
         super().__init__()
+
+    def remove_punctuation(self, raw_text: str) -> str:
+        return self.punctuation_pattern.sub(" ", raw_text)
+
+    def remove_stop_words(self, tokens: List[str]) -> List[str]:
+        return list(filter(lambda token: token not in self.stop_words, tokens))
+
+    def stem_tokens(self, tokens: List[str]) -> List[str]:
+        return list(map(lambda token: self.stemmer.stem(token), tokens))
+
+    def lemmatize_tokens(self, tokens: List[str]) -> List[str]:
+        return list(map(lambda token: self.lemmatizer.lemmatize(token), tokens))
 
     @abstractmethod
     def preprocess_text(self, raw_text: str) -> List[str]:
         pass
 
+    @abstractmethod
+    def tokenize_text(self, normalized_text: str) -> List[str]:
+        pass
+
+
+class EnglishTextPreprocessor(TextPreprocessor):
+    def __init__(
+        self,
+        stem: bool = True,
+        lemmatize: bool = False,
+        del_punctuation: bool = True,
+        del_stop_words: bool = True,
+        stemmer: str = "porter",
+    ):
+        super().__init__(stem, lemmatize, del_punctuation, del_stop_words)
+        if del_stop_words:
+            from nltk.corpus import stopwords
+
+            self.stop_words = set(stopwords.words("english"))
+        if lemmatize:
+            from nltk.stem import WordNetLemmatizer
+
+            self.lemmatizer = WordNetLemmatizer()
+        if stem:
+            if stemmer == "porter":
+                from nltk.stem import PorterStemmer
+
+                self.stemmer = PorterStemmer()
+            elif stemmer == "lancaster":
+                from nltk.stem import LancasterStemmer
+
+                self.stemmer = LancasterStemmer()
+
+    def tokenize_text(self, normalized_text: str) -> List[str]:
+        from nltk.tokenize import word_tokenize
+
+        return word_tokenize(normalized_text)
+
+    def preprocess_text(self, raw_text: str) -> List[str]:
+        if self.del_punctuation:
+            raw_text = self.remove_punctuation(raw_text)
+        tokens = self.tokenize_text(raw_text)
+        if self.stem:
+            tokens = self.stem_tokens(tokens)
+        if self.lemmatize:
+            tokens = self.lemmatize_tokens(tokens)
+        return tokens
+
 
 class PersianTextPreprocessor(TextPreprocessor):
     def __init__(
-        self, stem: bool = True, del_punctuation: bool = True, lemmatize: bool = False
+        self,
+        stem: bool = True,
+        lemmatize: bool = False,
+        del_punctuation: bool = True,
+        del_stop_words: bool = True,
     ):
-        super().__init__(stem, del_punctuation, lemmatize)
-        from hazm import Normalizer, WordTokenizer, Stemmer, Lemmatizer
+        super().__init__(stem, lemmatize, del_punctuation, del_stop_words)
+        from hazm import Normalizer, WordTokenizer, Stemmer, Lemmatizer, stopwords_list
 
-        self.punctuation_pattern = re.compile(
-            r'([؟!\?]+|\d[\d\.:\/\\]+\d|[:\.=,//،|}{؛»\]\)\}"«\[\(\{])'
-        )
+        if del_stop_words:
+            self.stop_words = set(stopwords_list())
         self.normalizer = Normalizer()
         self.word_tokenizer = WordTokenizer(separate_emoji=True)
         self.stemmer = Stemmer()
@@ -36,7 +107,7 @@ class PersianTextPreprocessor(TextPreprocessor):
 
     def preprocess_text(self, raw_text: str) -> List[str]:
         if self.del_punctuation:
-            raw_text = self.punctuation_pattern.sub(" ", raw_text)
+            raw_text = self.remove_punctuation(raw_text)
         normalized_text = self.normalize_text(raw_text)
         tokens = self.tokenize_text(normalized_text)
         if self.stem:
@@ -50,13 +121,3 @@ class PersianTextPreprocessor(TextPreprocessor):
 
     def tokenize_text(self, normalized_text: str) -> List[str]:
         return self.word_tokenizer.tokenize(normalized_text)
-
-    def remove_punctuation(self, tokens: List[str]) -> List[str]:
-        pattern = re.compile(r"[.!?;\\-]")
-        return list(filter(lambda token: not re.match(pattern, token), tokens))
-
-    def stem_tokens(self, tokens: List[str]) -> List[str]:
-        return list(map(lambda token: self.stemmer.stem(token), tokens))
-
-    def lemmatize_tokens(self, tokens: List[str]) -> List[str]:
-        return list(map(lambda token: self.lemmatizer.lemmatize(token), tokens))
