f40451c67ab1bcdbb729e66c5188b03c2a2d768a
Alireza Fatemi <alirezafa@rocketmail.com>
2020-05-13 19:33:11 +0430

    :bug: Fixed min mode.

--- src/utils/arg_k.py
+++ src/utils/arg_k.py
@@ -4,4 +4,4 @@ import numpy as np
 def arg_k(matrix: np.ndarray, k: int, find_max: bool = True):
     if find_max:
         return np.argpartition(matrix, -k, axis=0)[-k:, :]
-    return np.argpartition(matrix, k, axis=0)[-k:, :]
+    return np.argpartition(matrix, k, axis=0)[:k, :]
