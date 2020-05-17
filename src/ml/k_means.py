e2a2627ba4939a521403879cb761f9336ee6f1a1
Alireza Fatemi <alirezafa@rocketmail.com>
2020-05-17 12:09:49 +0430

    :sparkles: Return final labels in k_means.

--- src/ml/k_means.py
+++ src/ml/k_means.py
@@ -5,7 +5,9 @@ from numba import njit
 
 
 @njit(
-    nb.float32[:, :](nb.float32[:, :], nb.float32[:, :], nb.int64, nb.int64),
+    nb.types.Tuple((nb.float32[:, :], nb.int64[:]))(
+        nb.float32[:, :], nb.float32[:, :], nb.int64, nb.int64
+    ),
     parallel=True,
     fastmath=True,
 )
@@ -13,7 +15,7 @@ def k_means(features, initial_centroids, num_centers, num_iterations):
     centroids = initial_centroids
     num_samples, num_features = features.shape
     dist = np.zeros((num_samples, num_centers), dtype=np.float32)
-    for _ in range(num_iterations):
+    for iter in range(num_iterations + 1):
 
         for i in range(num_samples):
             for j in range(num_centers):
@@ -21,9 +23,12 @@ def k_means(features, initial_centroids, num_centers, num_iterations):
 
         labels = np.array([dist[i, :].argmin() for i in range(num_samples)])
 
+        if iter == num_iterations:
+            break
+
         for i in range(num_centers):
             centroids[i, :] = np.sum(features[labels == i, :], axis=0) / np.sum(
                 labels == i
             )
 
-    return centroids
+    return centroids, labels
